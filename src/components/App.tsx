852fca55ae039b237f426a26f7c764a6a5770140
alexandremendoncaalvaro <alexandre.alvaro@ambevtech.com.br>
2024-11-13 21:21:32 -0300

    fix: Button to center

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -535,12 +535,14 @@ const App: React.FC = () => {
                 processingResults={processingResults}
               />
               {selectedMarkerId && (
-                <button
-                  onClick={handleProcessVideo}
-                  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                >
-                  Process Selected Region
-                </button>
+                <div className="flex justify-center mt-4">
+                  <button
+                    onClick={handleProcessVideo}
+                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                  >
+                    Processar Região Selecionada
+                  </button>
+                </div>
               )}
             </div>
 
